350c0fa605f4cc0e07cc14a731906ff08937a667
murad <muradkv@gmail.com>
2019-06-07 21:18:51 +0300

    Add UI with constraints

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func changeFilter(_ sender: Any) {
+        
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
 }
 
