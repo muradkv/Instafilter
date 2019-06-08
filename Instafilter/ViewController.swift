745474dd7dc4347bc62501c99b2a455e3498702b
murad <muradkv@gmail.com>
2019-06-08 23:19:30 +0300

    Add filter for image with CoreImage

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     
     var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         addNavigationItem()
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
 
     @IBAction func changeFilter(_ sender: Any) {
@@ -27,6 +33,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     @IBAction func save(_ sender: Any) {
         
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
     }
     
     func addNavigationItem() {
@@ -48,6 +58,21 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
         currentImage = image
         
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
+        
+    }
+    
+    func applyProcessing() {
+        guard let image = currentFilter.outputImage else { return }
+        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        
+        if let cgimg = context.createCGImage(image, from: image.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            imageView.image = processedImage
+        }
     }
 }
 
